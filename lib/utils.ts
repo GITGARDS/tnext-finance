c3181d1f481416e0318d518d9187013cf6ed12e1
GITGARDS <adrianorogeriodesouza4353@gmail.com>
2025-01-17 18:11:49 -0300

    feat: Summary API

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,5 @@
-"use client";
 import { clsx, type ClassValue } from "clsx";
+import { eachDayOfInterval, isSameDay } from "date-fns";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -7,12 +7,12 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function convertAmountFromMiliunits(amount: number) {
-  return amount;
+  // return amount;
   return amount / 1000;
 }
 
 export function convertAmountToMiliunits(amount: number) {
-  return amount;
+  // return amount;
   return Math.round(amount * 1000);
 }
 
@@ -24,12 +24,37 @@ export function formatCurrency(value: number) {
   }).format(value);
 }
 
-export function calculatePercentageChange(
-  current: number,
-  previous: number
-) {
+export function calculatePercentageChange(current: number, previous: number) {
   if (previous === 0) {
     return previous === current ? 0 : 100;
   }
   return ((current - previous) / previous) * 100;
+}
+
+export function fillMissingDays(
+  activeDays: {
+    date: Date;
+    income: number;
+    expenses: number;
+  }[],
+  startDate: Date,
+  endDate: Date
+) {
+  if (activeDays.length === 0) {
+    return [];
+  }
+  const allDays = eachDayOfInterval({ start: startDate, end: endDate });
+  const transactionsByDay = allDays.map((day) => {
+    const found = activeDays.find((d) => isSameDay(d.date, day));
+    if (found) {
+      return found;
+    } else {
+      return {
+        date: day,
+        income: 0,
+        expenses: 0,
+      };
+    }
+  });
+  return transactionsByDay;
 }
